447ea5b8ad2f5703f8f6372b58db948eb727923a
ThaoNguyen2368 <23684241.thao@student.iuh.edu.vn>
2024-08-16 20:31:42 +0700

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Thao Nguyen");
+        System.out.println("Lop hoc phan DHKTPM19A");
+    }
+}
